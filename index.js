34a19fa2ee429779648099c45d638cb576dfbb8e
michael pearson <gravatar@x0rz.com>
2014-08-15 07:12:04 -0400

    npm bump

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  *
  * The Bipio Email Pod.  Outbound email delivery for Bips
  *
- * @author Michael Pearson <michael@cloudspark.com.au>
- * Copyright (c) 2010-2013 CloudSpark pty ltd http://www.cloudspark.com.au
+ * @author Michael Pearson <github@m.bip.io>
+ * Copyright (c) 2010-2013 Michael Pearson https://github.com/mjpearson
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
